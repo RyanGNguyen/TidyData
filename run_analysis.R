2505e0d370435642ac63e1a768fb0ce842404483
GitRyers <112345567+GitRyers@users.noreply.github.com>
2023-07-26 16:55:25 -0700

    Edited data

--- run_analysis.R
+++ run_analysis.R
@@ -5,7 +5,7 @@ run_analysis <- function() {
     library(tidyr)
     
     # Data Column Names
-    setwd("~/R/Coursera/Getting and Cleaning Data/TidyData/UCI HAR Dataset")
+    setwd("~/TidyData/UCI HAR Dataset")
     features <- readLines("features.txt")
     features <- gsub("^[0-9]+", "", features)
     features <- trimws(features)
@@ -13,7 +13,7 @@ run_analysis <- function() {
     colNames <- append(colNames, "Activity")
     
     # Loading Train Data
-    setwd("~/R/Coursera/Getting and Cleaning Data/TidyData/UCI HAR Dataset/train")
+    setwd("~/TidyData/UCI HAR Dataset/train")
     subject_train <- read.table("subject_train.txt")
     X_train <- read.table("X_train.txt")
     y_train <- read.table("y_train.txt")
@@ -23,7 +23,7 @@ run_analysis <- function() {
     names(train_data) <- colNames
     
     # Loading Test Data
-    setwd("~/R/Coursera/Getting and Cleaning Data/TidyData/UCI HAR Dataset/test")
+    setwd("~/TidyData/UCI HAR Dataset/test")
     subject_test <- read.table("subject_test.txt")
     X_test <- read.table("X_test.txt")
     y_test <- read.table("y_test.txt")
@@ -36,13 +36,12 @@ run_analysis <- function() {
     merged <- bind_rows(train_data, test_data)
     setorderv(merged, c("Subject", "Activity"), c(1,1))
     selected <- select(merged, c(1, grep("mean[^Freq]", colNames), grep("std", colNames), 563))
-    setwd("~/R/Coursera/Getting and Cleaning Data/TidyData/UCI HAR Dataset")
+    setwd("~/TidyData/UCI HAR Dataset")
     activities <- readLines("activity_labels.txt")
     activities <- gsub("^[0-9]+", "", activities)
     activities <- trimws(activities)
     labeled <- mutate(selected, Activity = activities[as.numeric(Activity)])
-    setwd("~/R/Coursera/Getting and Cleaning Data/TidyData")
-    save(labeled, file = "first_tidy_data.rda", compress = TRUE)
+    setwd("~/TidyData")
     
     # Second Dataset
     splitted <- split(labeled, by = c("Subject", "Activity"))
@@ -56,5 +55,5 @@ run_analysis <- function() {
         names(table) <- gsub("([X])(\\d+)(\\.)([A-Z]+)(_?)([A-Z]*)(\\.)", "", names(table))
         tidydata <- rbind(tidydata, table)
     }
-    save(tidydata, file = "second_tidy_data.rda", compress = TRUE)
+    write.table(tidydata, file = "tidy_data.txt", row.names = FALSE)
 }
